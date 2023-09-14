8c2e9e50f715e8a7b0d0660ffab9983d9b4208a3
Matheus Eler <matheus.eler@outlook.com>
2023-09-14 19:36:27 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+//Registra a classe como serviço
+@Service
+public class ProductService {
+
+	//Dependencia
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	
+	public Product findById (Long id) {
+		Optional<Product>obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
+	
