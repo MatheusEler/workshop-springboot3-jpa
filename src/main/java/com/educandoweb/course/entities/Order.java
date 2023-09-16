5452cdf2178f656ea8e2f8b12f04c5608bd6c495
Matheus Eler <matheus.eler@outlook.com>
2023-09-16 18:48:41 -0300

    OrderItem, many-to-many association with extra attributes  part2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,10 +2,13 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -35,6 +39,11 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	
 	public Order() {	
 	}
@@ -78,6 +87,11 @@ public class Order implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
